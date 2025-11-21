85678274a5234a9e1fa31025f71d9715e68d187f
Jesusvillarraga-jalasoft <jesus.villarraga@jalasoft.com>
2025-11-20 19:12:21 -0500

    updated sonar quality gate to latest

--- src/test/java/com/example/demo/CalculatorTest.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-class CalculatorTest{ @Test void testAdd(){ assertEquals(5,new Calculator().add(2,3)); }}
